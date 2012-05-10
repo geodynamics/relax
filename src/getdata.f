28448a4a8ef0b89a308ebe32453d045ee822933e
Walter Landry <wlandry@caltech.edu>
2012-05-10 07:02:18 -0700

    Replace tabs with spaces.

--- src/getdata.f
+++ src/getdata.f
@@ -1,30 +1,30 @@
-	subroutine getdata(unit,line)
-	implicit none
+        subroutine getdata(unit,line)
+        implicit none
 c
-c	First implemented in Potsdam, Feb, 1999
-c	Last modified: Potsdam, Nov, 2001, by R. Wang
+c       First implemented in Potsdam, Feb, 1999
+c       Last modified: Potsdam, Nov, 2001, by R. Wang
 c
-	integer unit
-	character line*256,char*1
+        integer unit
+        character line*256,char*1
 c
-	integer i
+        integer i
 c
-c	this subroutine reads over all comment lines starting with "#".
+c       this subroutine reads over all comment lines starting with "#".
 c
-	char='#'
-100	continue
-	if(char.eq.'#')then
-	  read(unit,'(a)')line
-	  i=1
-	  char=line(1:1)
-200	  continue
-	  if(char.eq.' ')then
-	    i=i+1
-	    char=line(i:i)
-	    goto 200
-	  endif
-	  goto 100
-	endif
+        char='#'
+100     continue
+        if(char.eq.'#')then
+          read(unit,'(a)')line
+          i=1
+          char=line(1:1)
+200       continue
+          if(char.eq.' ')then
+            i=i+1
+            char=line(i:i)
+            goto 200
+          endif
+          goto 100
+        endif
 c
-	return
-	end
+        return
+        end
