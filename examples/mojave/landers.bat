c1a299174b19192b50dd91a8ed91f30af7e28d38
Walter Landry <wlandry@caltech.edu>
2012-12-28 11:19:01 -0800

    Add landers.bat

---
+++ examples/mojave/landers.bat
@@ -0,0 +1,9 @@
+if not exist landers mkdir landers
+
+
+set OMP_NUM_THREADS=4
+
+set GMT_SHAREDIR=%CD%\..\..\share
+set PROJ_LIB=%CD%\..\..\share
+..\..\relax --no-vtk-output --no-proj-output --no-stress-output < landers.input
+PAUSE
