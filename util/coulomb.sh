51e64581eaeb25c4b7d5b9e485199469d39d2be0
sagar masuti <sagar.masuti@gmail.com>
2014-06-06 14:32:30 +0800

    Add rake information in coulomb stress change script. 	modified:   coulomb.sh

--- util/coulomb.sh
+++ util/coulomb.sh
@@ -4,22 +4,23 @@ set -e
 self=$(basename $0)
 trap 'echo $self: Some errors occurred. Exiting.; exit' ERR
 
-while getopts "d:s:" flag
+while getopts "d:s:r:" flag
 do
   case "$flag" in
     d) dset=1;dip=`echo "" | awk -v a=$OPTARG '{print a*3.1415926535897932385/180.0}'`;;
+    r) rset=1;rak=`echo "" | awk -v a=$OPTARG '{print a*3.1415926535897932385/180.0}'`;;
     s) sset=1;str=`echo "" | awk -v a=$OPTARG '{print a*3.1415926535897932385/180.0}'`;;
   esac
 done
-for item in $dset $sset;do
+for item in $dset $rset $sset;do
 	shift;shift
 done
 
 if (test $# -lt "1"); then
-        echo "$self projects a stress tensor along a hypothetic"
+    echo "$self projects a stress tensor along a hypothetic"
 	echo "fault defined by strike and dip orientation"
 	echo ""
-	echo "usage: $self -s strike -d dip index"
+	echo "usage: $self -s strike -d dip -r rake index"
 	echo ""
 	echo "a fault normal, dip and strike vectors are "
 	echo "n = [-sin(str) * sin(dip), +cos(str) * sin(dip), -cos(dip)]"
@@ -49,10 +50,11 @@ else
 	echo "$self: using dip angle "$dip "radian."
 fi
 if [ "$sset" != "1" ]; then
-	echo "$self: using default strike angle of 0 degrees."
 	str=0
-else
-	echo "$self: using strike angle "$str "radian."
+fi
+echo "$self: using strike angle "$str "radian."
+if [ "$rset" != "1" ]; then
+	rak=0
 fi
 
 while [ "$#" != "0" ];do
@@ -83,12 +85,14 @@ T3=$WDIR/$INDEX-t3.grd
 # cosine and sine of angles
 s1=`echo "" | awk -v d=$dip -v s=$str '{print cos(s) }'`
 s2=`echo "" | awk -v d=$dip -v s=$str '{print sin(s) }'`
-n1=`echo "" | awk -v d=$dip -v s=$str '{print +sin(s)*sin(d) }'`
-n2=`echo "" | awk -v d=$dip -v s=$str '{print -cos(s)*sin(d) }'`
+n1=`echo "" | awk -v d=$dip -v s=$str '{print -sin(s)*sin(d) }'`
+n2=`echo "" | awk -v d=$dip -v s=$str '{print +cos(s)*sin(d) }'`
 n3=`echo "" | awk -v d=$dip -v s=$str '{print -cos(d) }'`
 d1=`echo "" | awk -v d=$dip -v s=$str '{print +sin(s)*cos(d) }'`
 d2=`echo "" | awk -v d=$dip -v s=$str '{print -cos(s)*cos(d) }'`
 d3=`echo "" | awk -v d=$dip -v s=$str '{print -sin(d) }'`
+
+echo "$self: n=($n1,$n2,$n3), s=($s1,$s2,0), d=($d1,$d2,$d3)"
 
 grdmath $GRD11 $n1 MUL $GRD12 $n2 MUL ADD $GRD13 $n3 MUL ADD = $T1
 grdmath $GRD12 $n1 MUL $GRD22 $n2 MUL ADD $GRD23 $n3 MUL ADD = $T2
@@ -102,11 +106,11 @@ grdmath $T1 $n1 MUL $T2 $n2 MUL ADD $T3 $n3 MUL ADD = $GRDTN
 
 # strike component of traction
 # ts = (t1 - tn * n(1)) * s(1) + (t2 - tn * n(2)) * s(2)
-grdmath $T1 $GRDTN $n1 MUL SUB $s1 MUL $T2 $GRDTN $n2 MUL SUB $s2 MUL ADD = $GRDTS
+grdmath $T1 $GRDTN $n1 MUL SUB $s1 MUL $T2 $GRDTN $n2 MUL SUB $s2 MUL ADD $rak COS MUL = $GRDTS
 
 # dip component of traction
 # td = (t1 - tn * n(1)) * d(1) + (t2 - tn * n(2)) * d(2) + (t3 - tn * n(3)) * d(3)
-grdmath $T1 $GRDTN $n1 MUL SUB $d1 MUL $T2 $GRDTN $n2 MUL SUB $d2 MUL ADD $T3 $GRDTN $n3 MUL SUB $d3 MUL ADD = $GRDTD
+grdmath $T1 $GRDTN $n1 MUL SUB $d1 MUL $T2 $GRDTN $n2 MUL SUB $d2 MUL ADD $T3 $GRDTN $n3 MUL SUB $d3 MUL ADD $rak SIN MUL = $GRDTD
 
 shift
 if [ "$#" != "0" ];then
